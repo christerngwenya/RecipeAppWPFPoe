b21ef91c27e4769ca9003d1abdd9af9cfd6c7653
christerngwenya <168637426+christerngwenya@users.noreply.github.com>
2024-06-14 22:34:03 +0200

    Add files via upload

---
+++ RecipeAppWPF/IngredientWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RecipeAppWPF
+{
+    /// <summary>
+    /// Interaction logic for IngredientWindow.xaml
+    /// </summary>
+    public partial class IngredientWindow : Window
+    {
+        public Ingredient Ingredient { get; private set; }
+
+        public IngredientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(CaloriesTextBox.Text, out int calories))
+            {
+                Ingredient = new Ingredient(NameTextBox.Text, calories, FoodGroupTextBox.Text);
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid calories input");
+            }
+        }
+    }
+}
---
+++ RecipeAppWPF/Ingredients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeAppWPF
+{
+    public class Ingredient
+    {
+        public string Name { get; private set; }
+        public int Calories { get; private set; }
+        public string FoodGroup { get; private set; }
+
+        public Ingredient(string name, int calories, string foodGroup)
+        {
+            Name = name;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+    }
+}
